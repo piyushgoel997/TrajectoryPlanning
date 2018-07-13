4c3530a07937a7fd4f21fdefb6b7a97aeaca08a4
piyush <piyushgoel997@gmail.com>
2018-07-14 00:36:20 +0530

    added the code to train

---
+++ DataProc.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+
+
+def load_data(directory):
+    # TODO
+    return None, None
+
+
+def make_minibatches(X, Y, minibatch_size):
+    # TODO
+    return None, None
---
+++ Train.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+import argparse
+from Model import network
+import time
+from DataProc import load_data, make_minibatches
+
+
+def train(args):
+    # TODO make fns after the format of data is known
+    minibatchs_X, minibatchs_Y = make_minibatches(load_data(args.data_dir), args.minibatch_size)
+
+    tf.reset_default_graph()
+
+    X = tf.placeholder(tf.float32, shape=[None, 320, 162, 3], name='X')
+    # TODO change according to the number of output neurons.
+    Y = tf.placeholder(tf.float32, shape=[None, 1], name='Y')
+
+    pred = network(X)
+
+    with tf.name_scope('train'):
+        loss = tf.reduce_mean(tf.pow(Y - pred, 2))
+        tf.summary.scalar('loss', loss)
+        train_op = tf.train.AdamOptimizer().minimize(loss)
+
+    summary = tf.summary.merge_all()
+
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        if args.restore:
+            restore_path = tf.train.latest_checkpoint(args.save_dir)
+            saver.restore(sess, restore_path)
+            print('restored the model from' + str(restore_path))
+        else:
+            sess.run(init)
+
+        summary_writer = tf.summary.FileWriter(args.summary_dir, sess.graph)
+
+        train_start = time.time()
+        print("Training started")
+
+        for epoch in range(args.num_epochs):
+            epoch_start = time.time()
+
+            _loss, _, _summ = sess.run([loss, train_op, summary],
+                                       feed_dict={X: minibatchs_X[epoch], Y: minibatchs_Y[epoch]})
+
+            summary_writer.add_summary(_summ)
+
+            print("Epoch " + str(epoch + 1) +
+                  " completed in " + str(time.time() - epoch_start)[:5] +
+                  " secs with loss " + str(_loss))
+
+            if (epoch + 1) % 100 == 0:
+                save_path = saver.save(sess, args.save_dir + 'model.ckpt')
+                print("Model saved in the dir " + str(save_path))
+
+        print("Training Finished in " + str(time.time() - train_start)[:5])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # TODO change the number of epochs, batch size - if rqd.
+    parser.add_argument('--num_epochs', type=int, default=500)
+    parser.add_argument('--minibatch_size', type=int, default=128)
+    parser.add_argument('--restore', type=bool, default=False)
+    parser.add_argument('--data_dir', type=str)
+    parser.add_argument('--save_dir', type=str)
+    parser.add_argument('--summary_dir', type=str)
+
+    args = parser.parse_args()
+
+    train(args)
